837d7e0af85ee68f34d16264a6fe856935679007
Nikita2008 <niktazovod2008@gmail.com>
2025-10-03 18:22:45 +0300

    Зміни
    Оновлене головне меню та монстр

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public int Health;
+    public int MaxHealth = 300;
+    public int MinHealth = 0;
+}
